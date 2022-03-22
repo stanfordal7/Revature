abd553f22234210d30336216ec52dc7e753b6b96
Angela Stanford <angelastanford@Angelas-MacBook-Air.local>
2022-03-22 18:40:45 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int a = 5;
+int b = 6;
+if (a + b > 10)
+    Console.WriteLine("The answer is greater than 10.");
+
